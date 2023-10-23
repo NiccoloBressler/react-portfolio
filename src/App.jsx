6decda94cd06cae76d405206138c39bbd8a6b92b
Niccolo Bressler <bresslerniccolo@gmail.com>
2023-10-23 10:35:58 -0400

    Established styled-components container

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-function App() {
+import styled from "styled-components"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Experience from "./components/Experience"
+import Contact from "./components/Contact"
 
+const Container = styled.div`
+  height: 100vh;
+  background-color: black;
+`
+
+function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Hero/>
+      <About/>
+      <Projects/>
+      <Experience/>
+      <Contact/>
+    </Container>
   )
 }
 
